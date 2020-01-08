d565332ca30cbf38223f310173f9c7061fc8814f
kmlproduct <kam.salaheddine@gmail.com>
2020-01-08 23:16:10 +0100

    adding mouseover and onmouseover an onmouseout

--- Desktop/convertisseur/js/convert.js
+++ Desktop/convertisseur/js/convert.js
@@ -1,18 +1,23 @@
 
 function myConvert () {
+    
 
     var amount = document.getElementById("entrer").value;
         fromid = document.getElementById("id_from").value;
         fromto = document.getElementById("id_to").value;
         result = 0;
         
+        
 
-        if  (amount == 0 || isNaN(amount) || amount=="") {
+        if  (amount == 0 || isNaN(amount) || (fromid) == (fromto)){
             document.getElementById("message").innerHTML="should be write correct value";
             document.getElementById("message").style='background-color:red';
-            document.getElementById("id_from").value = document.getElementById("id_to").value;
+            
+        
+        
             return;
-        };
+            
+        }
         
         if (fromid == "euro"){
             switch (fromto){
@@ -48,7 +53,31 @@ function myConvert () {
         
         
         var message = amount + " "+fromid+" is " +result+ " "+fromto;
-        document.getElementById("message").style='background-color:rgb(75, 223, 82)';
-        document.getElementById("message").innerHTML=message;
+            document.getElementById("message").style='background-color:rgb(75, 223, 82)';
+            document.getElementById("message").innerHTML=message;
+           
+
+    
 }
+function myOnkey (){
+    
+    document.getElementById("message").style='background-color:yellow';
+    document.getElementById("message").innerHTML="write your value";
+}
+function myMouse(e) {
+    var x = e.clientX;
+    var y = e.clientY;
+    var coor = "Coordinates: (" + x + "," + y + ")";
+    
+    document.getElementById("message").innerHTML = coor;
+    document.getElementById("message").style='background-color:rgb(216, 204, 164)';
+    document.getElementById("entrer").innerHTML = " I will calculate your value  " ;
+  }
+  
+  function clearCoor() {
+    document.getElementById("message").innerHTML = "";
+    document.getElementById("message").style = "";
+    document.getElementById("entrer").innerHTML = "" ;
+    
+  }
 
