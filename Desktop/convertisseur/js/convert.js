7467be0bc6355c678a78a51a2e172e3c46a7c947
kmlproduct <kam.salaheddine@gmail.com>
2020-01-16 01:13:18 +0100

    adding convert distance and weiht

--- Desktop/convertisseur/js/convert.js
+++ Desktop/convertisseur/js/convert.js
@@ -1,3 +1,4 @@
+// convert devise
 
 function myConvert () {
     
@@ -45,7 +46,7 @@ function myConvert () {
                 case "euro":
                 result = amount*1.5;
                 break;
-                case"Dsollars":
+                case"Dollars":
                 result = amount * 0.5;
                 break;
             }
@@ -55,10 +56,171 @@ function myConvert () {
         var message = amount + " "+fromid+" is " +result+ " "+fromto;
             document.getElementById("message").style='background-color:rgb(75, 223, 82)';
             document.getElementById("message").innerHTML=message;
-           
-
-    
 }
+
+// convert devise 
+// convert distance
+function myDistance() {
+    
+
+    var amount = document.getElementById("up").value;
+        fromid = document.getElementById("id_fromDistance").value;
+        fromto = document.getElementById("id_toDistance").value;
+        result = 0;
+        
+        
+
+        if  (amount == 0 || isNaN(amount) || (fromid) == (fromto)){
+            document.getElementById("messageDistance").innerHTML="should be write correct value";
+            document.getElementById("messageDistance").style='background-color:red';
+            
+        
+        
+            return;
+            
+        }
+        
+        if (fromid == "km"){
+            switch (fromto){
+                case "m":
+                result = amount * 1000;
+                break;
+                case"cm":
+                result = amount * 100000;
+                break;
+                case "mm":
+                result = amount * 1000000;
+                break;
+                
+            }
+        }
+        if (fromid == "m"){
+            switch (fromto){
+                case "km":
+                result = amount / 1000;
+                break;
+                case"cm":
+                result = amount * 100;
+                break;
+                case "mm":
+                result = amount / 1000;
+                break;
+            }
+        }
+        if (fromid == "cm"){
+            switch (fromto){
+                case "m":
+                    result = amount / 100;
+                    break;
+                    case"km":
+                    result = amount / 100000;
+                    break;
+                    case "mm":
+                    result = amount / 1000000;
+                    break;
+            }
+        }
+        if (fromid == "mm"){
+            switch (fromto){
+                case "m":
+                    result = amount / 1000;
+                    break;
+                    case"cm":
+                    result = amount / 10000;
+                    break;
+                    case "mm":
+                    result = amount / 100000;
+                    break;
+            }
+        }
+        
+        
+        var message = amount + " "+fromid+" is " +result+ " "+fromto;
+            document.getElementById("messageDistance").style='background-color:rgb(75, 223, 82)';
+            document.getElementById("messageDistance").innerHTML=message;
+}
+// convertDistance
+
+// convertWeight
+function myWeight() {
+    
+    var amount = document.getElementById("entrerWeight").value;
+        fromid = document.getElementById("id_fromWeight").value;
+        fromto = document.getElementById("id_toWeight").value;
+        result = 0;
+        
+        
+
+        if  (amount == 0 || isNaN(amount) || (fromid) == (fromto)){
+            document.getElementById("messageWeight").innerHTML="should be write correct value";
+            document.getElementById("messageWeight").style='background-color:red';
+            
+        
+        
+            return;
+            
+        }
+        
+        if (fromid == "t"){
+            switch (fromto){
+                case "kg":
+                result = amount * 1000;
+                break;
+                case"g":
+                result = amount * 100000;
+                break;
+                case "mg":
+                result = amount * 1000000;
+                break;
+                
+            }
+        }
+        if (fromid == "kg"){
+            switch (fromto){
+                case "t":
+                result = amount / 1000;
+                break;
+                case"g":
+                result = amount * 1000;
+                break;
+                case "mg":
+                result = amount * 100000;
+                break;
+            }
+        }
+        if (fromid == "g"){
+            switch (fromto){
+                case "t":
+                    result = amount / 1000000;
+                    break;
+                    case"kg":
+                    result = amount / 1000;
+                    break;
+                    case "mg":
+                    result = amount * 100;
+                    break;
+            }
+        }
+        if (fromid == "mg"){
+            switch (fromto){
+                case "t":
+                    result = amount / 1000000;
+                    break;
+                    case"kg":
+                    result = amount / 10000;
+                    break;
+                    case "g":
+                    result = amount / 100;
+                    break;
+            }
+        }
+        
+        
+        var message = amount + " "+fromid+" is " +result+ " "+fromto;
+            document.getElementById("messageWeight").style='background-color:rgb(75, 223, 82)';
+            document.getElementById("messageWeight").innerHTML=message;
+}
+// convertWeight
 function myOnkey (){
     
     document.getElementById("message").style='background-color:yellow';
@@ -80,4 +242,21 @@ function myMouse(e) {
     document.getElementById("entrer").innerHTML = "" ;
     
   }
+  
+function myDistanceBlock() {
+    var myDisplay = document.getElementById("Distance").style.display = "block";
+    var myDisplay = document.getElementById("Devise").style.display = "none";
+    var myDisplay = document.getElementById("Weight").style.display = "none";
+      
+}
+function myDevisBlock(){
+    var myDisplay = document.getElementById("Distance").style.display = "none";
+    var myDisplay = document.getElementById("Devise").style.display = "block";
+    var myDisplay = document.getElementById("Weight").style.display = "none";
+}
+function myWeightBlock() {
+    var myDisplay = document.getElementById("Distance").style.display = "none";
+    var myDisplay = document.getElementById("Devise").style.display = "none";
+    var myDisplay = document.getElementById("Weight").style.display = "block";
+}
 
